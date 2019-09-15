738fc6b7f7442a9a219a449bf81611dec2614d64
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-09-15 12:48:57 +0100

    fix

--- slack.js
+++ slack.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) =>{
+    res.send('Hello')
+})
 const server = app.listen(9000);
 
 const io = socketIO(server);
